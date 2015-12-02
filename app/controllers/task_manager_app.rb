387908b487624590cf991828c496c8c76c2aec20
Brant Wellman <BrantWellman@gmail.com>
2015-12-02 09:32:53 -0700

    Minor changes

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,27 +2,51 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
   end
 
+  # read
   get '/tasks' do
     @tasks = TaskManager.all
     erb :index
   end
 
+  # create(first-half)
   get '/tasks/new' do
     erb :new
   end
 
+  # create(2nd-half)
   post '/tasks' do
     TaskManager.create(params[:task])
     redirect '/tasks'
   end
 
+  # read
   get '/tasks/:id' do |id|
     @task = TaskManager.find(id.to_i)
     erb :show
+  end
+
+  get '/tasks/:id/edit' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    TaskManager.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    TaskManager.delete(id.to_i)
+    redirect '/tasks'
+  end
+
+  not_found do
+    erb :error
   end
 end
